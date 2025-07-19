a0cd5202ed75fbb6ff5990f46ea312255dc3e491
shahd Hussin <shahdm781@gmal.com>
2025-07-19 22:52:58 +0300

    fix: move useContext to UserDetails.jsx and cleaned up props

--- alx-react-app-props/src/ProfilePage.jsx
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import UserInfo from './UserInfo';
 
-function ProfilePage({ userData }) {
-    return <UserInfo userData={userData} />;
+function ProfilePage() {
+    return <UserInfo />;
 }
 
 export default ProfilePage;
